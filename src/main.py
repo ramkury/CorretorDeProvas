d8478593ee002d092ffe54ad2cdb3f905d99e878
Ricardo <ricardokury@outlook.com>
2017-06-12 14:12:06 -0300

    Renomeia imagens da prova

---
+++ src/main.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+
+def main():
+    prova = []
+    prova.append(cv2.imread('../img/p1_1.png'))
+    prova.append(cv2.imread('../img/p1_2.png'))
+    prova.append(cv2.imread('../img/p1_3.png'))
+
+
+if __name__ == '__main__':
+    main()
